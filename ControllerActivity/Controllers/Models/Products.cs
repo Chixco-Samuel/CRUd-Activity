eb34d031bff258a06730d7d07bd8e2c26bfded16
Chico Samuel C Santos <chico.santos.college@ustangelicum.edu.ph>
2026-08-28 21:57:40 +0800

    Add project files.

---
+++ ControllerActivity/Controllers/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace ControllerActivity.Controllers.Models
+{
+    public class Product
+    {
+        public int ProductId {  get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Id { get; internal set; }
+    }
+}
---
+++ ControllerActivity/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ControllerActivity.Controllers.Models;
+namespace ControllerActivity.Controllers
+{
+    [Route("Production")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public static List<Product> products = new List<Product>();
+
+        public ProductsController()
+        {
+
+        }
+
+        [HttpGet]
+        public List<Product> GetProduct()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = products.Where(p => p.ProductId == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(Product product)
+        {
+            products.Add(product);
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Product> EditPerson(int id, Product newproduct)
+        {
+            var product = products.Where(m => m.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product = newproduct;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeletePerson(int id)
+        {
+            var product = products.Where(m => m.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+            return Ok();
+        }
+    }
+}
